453a51191be5a05ed59179ab2f37a028797cd060
JosephcatZ <legojosephcatz@gmail.com>
2019-08-16 18:51:55 -0400

    HIO, we are done setting up, so we wont need you anny more, bye bye

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
-print("Hello, World!")
+board = [
+            [' ',' ',' '],
+            [' ',' ',' '],
+            [' ',' ',' ']
+        ]
+Turn = 'X'
+def drawBoard(board):
+    y = 0
+    for i in board:
+        out = ''
+        for j in i:
+            out = out + str(j)+'|'
+        print(out[0:5])
+        y=y+1
+        if y < len(board):
+            print("-+-+-")
+
+drawBoard(board)    
